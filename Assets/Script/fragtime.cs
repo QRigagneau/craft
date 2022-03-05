10d9982ae3ddf9d8f879662fb93414c080b52cfa
QRigagneau <92300152+QRigagneau@users.noreply.github.com>
2022-03-05 22:22:21 +0100

    Add sound to collectible

--- Assets/Script/fragtime.cs
+++ Assets/Script/fragtime.cs
@@ -7,10 +7,17 @@ public class fragtime : MonoBehaviour
     public int timetravel;
     private GameObject player;
 
+    private GameObject collectiblesound;
+
+    public AudioClip collectible;
+    public AudioSource audiosource;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("player");
+        collectiblesound = GameObject.Find("collectiblesound");
+        audiosource = collectiblesound.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,11 +27,12 @@ public class fragtime : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.GetComponent<CharacterMove>())
+        if (collision.gameObject.GetComponent<CharacterMove>())
         {
             player.GetComponent<LifeController>().age += timetravel;
-            isUseCollectible();
+            isUseCollectible();            
         }
+        PlayCollectibleSound();
     }
 
     public void isUseCollectible()
@@ -35,5 +43,10 @@ public class fragtime : MonoBehaviour
     public void refreshCollectible()
     {
         gameObject.SetActive(true);
+    }
+
+    public void PlayCollectibleSound()
+    {
+        audiosource.PlayOneShot(collectible);
     }
 }
