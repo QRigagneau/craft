e7ce5568e977d2a9b352ab3d1df94a80fec21070
valentin malvoisin <v.malvoisin@lahorde.school>
2022-03-05 12:18:29 +0100

    Animation

--- Assets/Scenes/Valentin/ScriptVal/CharacterMove.cs
+++ Assets/Scenes/Valentin/ScriptVal/CharacterMove.cs
@@ -8,19 +8,42 @@ public class CharacterMove : MonoBehaviour
     public float speed = 2.5f;
     Rigidbody2D rb;
     Vector2 dir;
-
+    Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     
     void Update()
-    {
+    {       
         dir.x = Input.GetAxisRaw("Horizontal");
         dir.y = Input.GetAxisRaw("Vertical");
 
         rb.MovePosition(rb.position + dir * speed * Time.fixedDeltaTime);
+        setParam();
+    }
+
+    void setParam()
+    {
+        if (dir.x == 0 && dir.y == 0)//static
+            anim.SetInteger("dir", 0);
+        else if (dir.y < 0) //bas
+            anim.SetInteger("dir", 1);
+        else if (dir.y > 0) //haut
+            anim.SetInteger("dir", 3);
+        else if (dir.x > 0) //droite
+        {
+            anim.SetInteger("dir", 2);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        else if (dir.x < 0) //gauche
+        {
+            anim.SetInteger("dir", 2);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 }
