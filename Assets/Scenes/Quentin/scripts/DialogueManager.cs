a7baa138716fbcd5d2b2f9e138936ca7e10a21da
QRigagneau <92300152+QRigagneau@users.noreply.github.com>
2022-03-05 11:13:51 +0100

    Add Dialogue finish

--- Assets/Scenes/Quentin/scripts/DialogueManager.cs
+++ Assets/Scenes/Quentin/scripts/DialogueManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+
+    public Text nameText;
+    public Text dialogueText;
     private Queue<string> sentences;
     void Start()
     {
@@ -11,7 +15,32 @@ public class DialogueManager : MonoBehaviour
     }
        
     public void StartDialogue (Dialogue dialogue)
+    { 
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
     {
-        Debug.Log("Nous commençons une conversation" + dialogue.name);
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
     }
 }
